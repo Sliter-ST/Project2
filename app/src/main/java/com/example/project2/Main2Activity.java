d8f41727a6bfd8ce08f92baccba7d6e9eaeab72c
Sliter-ST <yuzifeng1216@qq.com>
2019-12-02 18:49:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/project2/Main2Activity.java
@@ -0,0 +1,21 @@
+package com.example.project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("extra_data");
+        Log.d("Main2Activity",data);
+    }
+}
